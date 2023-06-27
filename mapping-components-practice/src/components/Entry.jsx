fc63f9c35bbf0f12de8266238c6905004dea278a
yusufby <yusufbayram19891989@gmail.com>
2023-06-27 09:30:22 +0100

    mapping data

---
+++ mapping-components-practice/src/components/Entry.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Entry(props) {
+  return (
+    <dl className="dictionary">
+      <div className="term">
+        <dt>
+          <span className="emoji" role="img" aria-label="Tense Biceps">
+            {props.emoji}
+          </span>
+          <span>{props.name}</span>
+        </dt>
+        <dd>{props.description}</dd>
+      </div>
+    </dl>
+  );
+}
+export default Entry;
---
+++ mapping-data-to-components/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+import Name from "./Name";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <p>{props.id}</p>
+        <Name name={props.name} />
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
