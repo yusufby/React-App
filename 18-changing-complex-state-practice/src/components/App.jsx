32a101bc8b90569e7b6f2255dbe1480d5c3859d1
yusufby <yusufbayram19891989@gmail.com>
2023-07-05 09:53:53 +0100

    es6 spread operator

---
+++ 18-changing-complex-state-practice/src/components/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setContact((previousValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: previousValue.lName,
+          email: previousValue.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: previousValue.fName,
+          lName: value,
+          email: previousValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: previousValue.fName,
+          lName: previousValue.lName,
+          email: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit </button>
+      </form>
+    </div>
+  );
+}
+export default App;
